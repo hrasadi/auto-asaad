983c74cd983e5c530a7dafc3d0be4c86c2e13039
Hamid <ubuntu@ip-172-31-52-113.ec2.internal>
2017-07-02 16:27:24 -0400

    admins tools

--- radio/admin-tools/get-lineup.js
+++ radio/admin-tools/get-lineup.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var moment = require('moment');
+var path = require('path');
 
 // Entry Point
 if (process.argv.length < 4) {
@@ -7,11 +8,11 @@ if (process.argv.length < 4) {
 	process.exit(1);
 }
 
-var cwd = path.resolve(path.dirname(process.argv[3]));
-var radioId = process.argv[4];
+var cwd = path.resolve(path.dirname(process.argv[2]));
+var radioId = process.argv[3];
 var today = moment().format("YYYY-MM-DD");
 
 var lineupPath = cwd + "/lineups/" + radioId + "-" + today + ".json.compiled";
-var compiledLineup = JSON.parse(self.fs.readFileSync(lineupPath, 'utf8'));
+var compiledLineup = JSON.parse(fs.readFileSync(lineupPath, 'utf8'));
 
 console.log(JSON.stringify(compiledLineup, null, 2));
