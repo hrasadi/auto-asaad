0c7c83c02e5fe0178b78e04cbab51e197553393a
Hamid <hr.asaadi@nexeven.com>
2017-07-05 01:35:53 -0400

    new queue model final touches

--- radio/liquidsoap/update-lineup-file.sh
+++ radio/liquidsoap/update-lineup-file.sh
@@ -2,10 +2,6 @@
 
 { echo "var.set current_lineup_file = \"${1}\""; sleep 1; } | telnet localhost 1234 
 
-# Save where in the lineup is the first program that sould be played
+# Save what program is currently played (used nowadays for the filler)
 echo "${2}" > "${1}.program.iter"
 
-# reset the iterators
-rm "${1}.preshow.iter"
-rm "${1}.show.iter"
-rm "${1}.show.lock"
