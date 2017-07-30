9840402237f4389ed37cefdf5aa1b07ac95b50a4
Hamid <hr.asaadi@nexeven.com>
2017-07-29 20:47:09 -0400

    indentation fixes

--- utils.js
+++ utils.js
@@ -1,34 +1,33 @@
 var Utils = function() {
-	this.http = require('http');
+    this.http = require('http');
 }
 
 Utils.prototype.inheritsFrom = function(child, parent) {
-	child.prototype = Object.create(parent.prototype);
-	// re-register the constructor
-	child.prototype.constructor = child;
+    child.prototype = Object.create(parent.prototype);
+    // re-register the constructor
+    child.prototype.constructor = child;
 }
 
 
 Utils.prototype.httpGet = function(hostname, path, callback_fn) {
     var getParams = {
-		    hostname: null,
-			port: 80,
-			path: null,
-			agent: false  // create a new agent just for this one request
-	};
-	getParams.hostname = hostname;
-	getParams.path = path;
+        hostname: null,
+        port: 80,
+        path: null,
+        agent: false  // create a new agent just for this one request
+    };
+    getParams.hostname = hostname;
+    getParams.path = path;
 
     this.http.get(getParams, function(response) {
-		    var body = '';
-		    response.on('data', function(d) {
-			    body += d;
-			});
-		    response.on('end', function() {
-		    	callback_fn(body);
-			});
-		}
-	);
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            callback_fn(body);
+        });
+    });
 }
 
 module.exports = new Utils();
