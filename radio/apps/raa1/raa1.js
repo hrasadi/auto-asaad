570aa29fe858969802e142222c01cb39cc787094
Hamid <hr.asaadi@nexeven.com>
2017-07-29 20:47:09 -0400

    indentation fixes

--- radio/apps/raa1/raa1.js
+++ radio/apps/raa1/raa1.js
@@ -31,9 +31,9 @@ var Raa1 = function(configFile, deploymentMode) {
 Utils.inheritsFrom(Raa1, Radio);
 
 Raa1.prototype.initialize = function() {
-    
+
     this.config = JSON.parse(fs.readFileSync(this.configFilePath, 'utf8'));
-    
+
     if (this.config == null) {
         console.log("FATAL ERROR: Error reading config file");
         process.exit(1);
@@ -51,22 +51,22 @@ Raa1.prototype.reset = function(currentDate, callback_fn) {
 
     var self = this;
     this.events.readTodayEvent(function(eventsDict) {
-        self.dataProvider.fajrTime = events[self.events.EventType.FAJR];    
-        self.dataProvider.dhuhrTime = events[self.events.EventType.DHUHR];    
-        self.dataProvider.maghribTime = events[self.events.EventType.MAGHRIB];    
-        
+        self.dataProvider.fajrTime = events[self.events.EventType.FAJR];
+        self.dataProvider.dhuhrTime = events[self.events.EventType.DHUHR];
+        self.dataProvider.maghribTime = events[self.events.EventType.MAGHRIB];
+
         // give back control to lineupManager
         callback_fn();
-    }); 
+    });
 }
 
 Raa1.prototype.calculateProgramStartTime = function(program) {
     if (program.Id == 'FajrProgram') {
-        return this.dataProvider.fajrTime;       
+        return this.dataProvider.fajrTime;
     } else if (program.Id == 'DhuhrProgram') {
-        return this.dataProvider.dhuhrTime;       
+        return this.dataProvider.dhuhrTime;
     } else if (program.Id == 'MaghribProgram') {
-        return this.dataProvider.maghribTime;       
+        return this.dataProvider.maghribTime;
     }
 }
 
@@ -82,7 +82,7 @@ Radio.prototype.onLineupCompiled = function(compiledLineup) {
     data.array = [];
     for (var i = 0; i < compiledLineup.Programs.length; i++) {
         var entry = {};
-    
+
         if (compiledLineup.Programs[i].Title) {
                     entry.title = compiledLineup.Programs[i].Title;
                 } else {
@@ -100,7 +100,7 @@ Radio.prototype.onLineupCompiled = function(compiledLineup) {
                 }
             }
         } else {
-            entry.time = moment(compiledLineup.Programs[i].Show.Meta.TentativeStartTime).format('HH:mm');           
+            entry.time = moment(compiledLineup.Programs[i].Show.Meta.TentativeStartTime).format('HH:mm');
         }
         for (var j = 0; j < compiledLineup.Programs[i].Show.Clips.length; j++) {
             if (compiledLineup.Programs[i].Show.Clips[j].Description) {
@@ -110,9 +110,9 @@ Radio.prototype.onLineupCompiled = function(compiledLineup) {
                     vodUrl = new URL(vodRelativeURI, 'http://vod.raa.media/');
                     entry.description += '<a class="vod-link" href="' + vodUrl.toString() + '">' + compiledLineup.Programs[i].Show.Clips[j].Description + "</a>";
                 } else {
-                    entry.description += compiledLineup.Programs[i].Show.Clips[j].Description;                
-                }               
-                
+                    entry.description += compiledLineup.Programs[i].Show.Clips[j].Description;
+                }
+
                 if (j != compiledLineup.Programs[i].Show.Clips.length - 1) { // Except for the last item
                     entry.description += "؛ ";
                 }
@@ -135,37 +135,3 @@ if (process.argv.length < 4) {
 var raa1 = new Raa1(process.argv[2], process.argv[3]);
 
 raa1.initialize();
-
-
-/*
-
-events.readTodayEvent(events.EventType.MAGHRIB, function(maghribTime) {
-
-        var indexDayOfRamadan = moment(maghribTime).diff(moment(config.RamadanStartDate), 'days');
-        var lineupName = 'ramadan17-lineup-' + moment(maghribTime).format("YYYY-MM-DD");
-
-        var lineup = lineupManager.createLineup(maghribTime, indexDayOfRamadan, lineupName);
-
-        var preAdhanProgramTime = moment(maghribTime).subtract(lineup.totalPreProgramLineupDuration, 'seconds').set('second', 0);
-
-        // Now let's create auto-asaad message
-        scheduleAutoAsaadMessage(lineup, indexDayOfRamadan, maghribTime, preAdhanProgramTime);
-    });
-
-var scheduleAutoAsaadMessage = function(lineup, indexDayOfRamadan, adhanTime, preAdhanProgramTime) {
-    var autoAsaadMessage = "برنامه‌های رادیو اتو اسعد، " + (indexDayOfRamadan + 1) + " رمضان المبارک\\n";
-
-    autoAsaadMessage += "شروع برنامه‌ها از ساعت: " + moment(preAdhanProgramTime).format("HH:mm") + "\\n";
-
-    autoAsaadMessage += "ابتدا به " + lineup.preProgramPlaylist[0].description + " گوش فرا خواهیم داد. سپس نوای " + lineup.preProgramPlaylist[1].description
-                + " را خواهیم شنید. بعد از " + lineup.preProgramPlaylist[2].description + " با شنیدن " + lineup.preProgramPlaylist[3].description 
-                + " به استقبال " + lineup.programPlaylist[0].description + " خواهیم رفت. در نهایت " + lineup.programPlaylist[1].description 
-                + " پایان بخش برنامه‌های امشب رادیو اتو-اسعد خواهد بود. \\n التماس دعا."
-
-    var calEventDateTime = moment(adhanTime).set('hour', 14).set('minute', 0).format('YYYY-MM-DDTHH:mm:ss');
-    messaging.createCalEvent(calEventDateTime, "#scheduler", autoAsaadMessage);
-}
-*/
-
-
-
