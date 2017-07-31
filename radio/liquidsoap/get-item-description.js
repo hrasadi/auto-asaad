74b050b12e1d988bf225ab9324b32ffeae965dc8
Hamid <hr.asaadi@nexeven.com>
2017-07-31 13:19:41 -0400

    issue #9 fixed

--- radio/liquidsoap/get-item-description.js
+++ radio/liquidsoap/get-item-description.js
@@ -10,7 +10,17 @@ if (fs.existsSync(lineupFilePath)) {
 	lineup = JSON.parse(fs.readFileSync(lineupFilePath, 'utf8'));
 
 	var currentProgramIdx = parseInt(fs.readFileSync(lineupFilePath + ".program.iter", 'utf8'));
+	
+	// In case of back-to-back programs, we need to move program iter when the first 
+	// track of the next programs begins to play.
+	if (lineup.Programs[currentProgramIdx + 1] && 
+		string.indexOf(lineup.Programs[currentProgramIdx].Show.Clips[0], currentTrackFilePath) != -1) {
+		// Lets shift the current program iterator
+		currentProgramIdx++;
+		fs.writeFileSync(lineupFilePath + ".program.iter", currentProgramIdx);
+	}
+
 	console.log(lineup.Programs[currentProgramIdx].Title);
 		
-	// TODO use currentTrackFilePath to show th current track info too
+	// TODO use currentTrackFilePath to show the current track info too
 }
