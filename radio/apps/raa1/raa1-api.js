5a52fb662c63ceb4b6299b6653919c18c04c8ffa
Hamid <hr.asaadi@nexeven.com>
2018-01-30 16:00:12 -0500

    user registering debug

--- radio/apps/raa1/raa1-api.js
+++ radio/apps/raa1/raa1-api.js
@@ -79,7 +79,7 @@ class Raa1API extends AppContext {
         let self = this;
         this._webApp.post('/registerDevice/:deviceType/', (req, res) => {
             let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-            let user = new User(request.body, req.params.deviceType, ip);
+            let user = new User(req.body, req.params.deviceType, ip);
             self.UserManager.registerUser(user);
             res.send('Success');
         });
