c7ae1f7247f449c4e1c16e1d207fc0dd1b608d99
Hamid <hr.asaadi@nexeven.com>
2018-02-04 00:50:42 -0500

    minor bug fix

--- radio/collaborativelistening/PublicFeed.js
+++ radio/collaborativelistening/PublicFeed.js
@@ -68,7 +68,7 @@ class PublicFeed extends Feed {
 
         // Delete any entries with same Id exists from before (old onces)
         // We will continue on complete callback from deregister (note async func)
-        this.deregisterFeedEntry(feedEntry);
+        this.deregisterFeedEntry(new PublicFeedEntry(feedEntry));
 
         if (AppContext.getInstance('LineupGenerator').GeneratorOptions.TestMode) {
             AppContext.getInstance().Logger.debug(
